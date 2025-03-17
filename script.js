7bb815417e8635a7b3c869e3f7cf0a6e072673ed
Durga-velu <durgadevi134babu@gmail.com>
2025-03-18 00:33:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 function toCase(text) {
   // write your code here
-	let ans=text.toUpperCase()
+	
 	let res=text.toLowerCase()
-	let result=ans.concat(-,res)
+	let ans=text.toUpperCase()
+	let result=ans.concat("-",res)
 	return result
 }
 
 // DO not change the code below
-
 const text = prompt("Enter text:");
 alert(toCase(text));
