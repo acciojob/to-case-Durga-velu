d53239cea5c42079fbaf3fd407461008628d750a
Durga-velu <durgadevi134babu@gmail.com>
2025-03-18 00:31:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 function toCase(text) {
   // write your code here
+	let ans=text.toUpperCase()
+	let res=text.toLowerCase()
+	let result=ans.concat(-,res)
+	return result
 }
 
 // DO not change the code below
